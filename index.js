b6000fe197f0e17993628d5a0281d9de63dad532
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-28 19:28:22 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+
+const submitData =(userName,userEmail)=>{
+    let dataSubmitted={
+        name:userName,
+        email:userEmail
+
+            };
+            let userData=  JSON.stringify(dataSubmitted);
+
+
+
+        let fetchRes=fetch(`http://localhost:3000/users`,{
+            method:"POST",
+            headers:{
+            "Content-Type":"application/json",
+            Accept:"application/json"
+            },
+            body:userData,
+            });
+
+        fetchRes.then((response)=> response.json())
+        .then((data)=>{
+
+            document.body.innerHTML=data[`id`];
+
+        })
+        .catch(function (error) {
+
+            document.body.innerHtml=error.message
+
+          });
+
+
+          return fetchRes;
+}
+
